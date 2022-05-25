498a3e65b41292c3240874e62245f0e8247c43f4
unknown <galamouyal88@gmail.com>
2022-05-25 11:31:39 +0300

    typescript

---
+++ examples/typescript/dist/index.js
@@ -0,0 +1,7 @@
+var userName = "galamo";
+console.log(userName);
+var user = { name: "lynn", age: 22 };
+console.log(user);
+console.log(user);
+console.log(user);
+console.log(user);
---
+++ examples/typescript/src/index.ts
@@ -0,0 +1,12 @@
+const userName: string = "galamo";
+console.log(userName);
+interface IUser {
+  name: string;
+  age: number;
+}
+
+const user: IUser = { name: "lynn", age: 22 };
+console.log(user);
+console.log(user);
+console.log(user);
+console.log(user);
